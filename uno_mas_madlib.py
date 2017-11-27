2e9cc5c8bc67122e79e600fc377ead28d6e4adba
ChinoALM <mortona35@gmail.com>
2017-11-27 11:04:57 -0500

    def check_answers(answer) checks user input and tallies strike count

--- uno_mas_madlib.py
+++ uno_mas_madlib.py
@@ -4,9 +4,11 @@ blanks = ["__1__", "__2__", "__3__", "__4__", "__5__"]
 easy_madlib = """Hey __1__ , dont make it __2__ .  Take a sad __3__ and make it better.
 Remember to let __4__ into you heart. Then you can __5__ to make it better."""
 easy_answers = ['Jude', 'bad', 'song', 'her', 'start']
+
 medium_madlib = """Let __1__ take you down cause Im going to __2__ fields.
 Nothing is __3__ , and __4__ to get hung about. Strawberry __5__ forever."""
 medium_answers =  ['me', 'strawberry', 'real', 'nothing', 'fields']
+
 hard_madlib = """There is nothing __1__ can do that cant be done. Nothing you can __2__ that
 cant be __3__ . Nothing you can say but you can __4__ how to play the game. Its __5__ . All you
 need is love."""
@@ -22,3 +24,21 @@ def fill_in_blanks(word, blanks):
             return blank
     return None
 
+def check_answers(answer):
+    """
+    Prompts user input and compares user input to answers list to check if guess is correct.
+    User is informed: correct or incorrect; and receives three strikes (attempts) per blank.
+    """
+    check = False
+    while check == False:
+        strike_count = 3
+        for strike in range(strike_count):
+            user_input = raw_input("What is the correct answer? ")
+            if user_input == answer:
+                print "You are correct!"
+                return answer
+            else:
+                print "Incorrect. Please guess again."
+            if strike == strike_count - 1:
+                print "The correct answer is " + answer + "."
+                return answer
