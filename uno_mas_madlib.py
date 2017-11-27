93c8eeaf4d68920e816672634cff0d850da53e20
ChinoALM <mortona35@gmail.com>
2017-11-27 01:14:24 -0500

    blanks, madlib levels and corresponding answers in an index

---
+++ uno_mas_madlib.py
@@ -0,0 +1,13 @@
+blanks = ["__1__", "__2__", "__3__", "__4__", "__5__"]
+
+#madlib_levels and answers
+easy_madlib = """Hey __1__ , dont make it __2__ .  Take a sad __3__ and make it better.
+Remember to let __4__ into you heart. Then you can __5__ to make it better."""
+easy_answers = ['Jude', 'bad', 'song', 'her', 'start']
+medium_madlib = """Let __1__ take you down cause Im going to __2__ fields.
+Nothing is __3__ , and __4__ to get hung about. Strawberry __5__ forever."""
+medium_answers =  ['me', 'strawberry', 'real', 'nothing', 'fields']
+hard_madlib = """There is nothing __1__ can do that cant be done. Nothing you can __2__ that
+cant be __3__ . Nothing you can say but you can __4__ how to play the game. Its __5__ . All you
+need is love."""
+hard_answers = ['you', 'sing', 'sung', 'learn', 'easy']
